f1ef6e192dab7321bfa49fe8505646349c32d25b
Alex <alex@Alexs-MacBook-Pro.local>
2020-03-25 17:33:00 -0400

    1st commit

---
+++ ETF_screener.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 16:13:38 2020
+
+@author: alex
+"""
+
+import yfinance as yf
+
+data = yf.download(tickers = "ZSP.TO XIU.TO XIC.TO ZAG.TO XSP.TO ZCN.TO XBB.TO XEF.TO")
+
+type(data)
+                   
+ZEA.TO
+VAB.TO
+VFV.TO
+ZLB.TO
+PSA.TO
+VUN.TO
+VCN.TO
+XSB.TO
+XUS.TO
+HXT.TO
+XCB.TO
+ZPR.TO
+ZUE.TO
+ZWB.TO
+ZLU.TO
+ZFL.TO
+CSAV.TO
+PMIF.TO
+ZIC.TO
+XRE.TO
+ZMU.TO
+XDV.TO
