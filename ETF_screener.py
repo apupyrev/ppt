5f325beeb09346094299e9329acded597b1726e5
alex <alex.pupyrev@gmail.com>
2020-04-04 19:57:10 -0400

    upd

--- ETF_screener.py
+++ ETF_screener.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 25 16:13:38 2020
-
-@author: alex
-"""
+# Created on Wed Mar 25 16:13:38 2020
 
 # %%
 # Packages and Imports
