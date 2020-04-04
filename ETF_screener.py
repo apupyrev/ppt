3bb074e7ac2b85fdee6385f2d6ce2ff6bc2d47b6
alex <alex.pupyrev@gmail.com>
2020-04-04 19:19:10 -0400

    symbol universe

--- ETF_screener.py
+++ ETF_screener.py
@@ -7,23 +7,20 @@ Created on Wed Mar 25 16:13:38 2020
 """
 
 # %%
+# Packages and Imports
+
 import yfinance as yf
 
-
 # %%
+# Symbols universe
 
-cad_etfs_top50_cap = ["ZSP.TO","XIU.TO","XIC.TO","ZAG.TO","XSP.TO","ZCN.TO","XBB.TO",
-"XEF.TO","ZEA.TO","VAB.TO","VFV.TO","ZLB.TO","PSA.TO","VUN.TO","VCN.TO","XSB.TO",
-"XUS.TO","HXT.TO","XCB.TO","ZPR.TO","ZUE.TO","ZWB.TO","ZLU.TO","ZFL.TO","CSAV.TO",
-"PMIF.TO","ZIC.TO","XRE.TO","ZMU.TO","XDV.TO","VIU.TO","HPR.TO","ZCS.TO","ZEM.TO",
-"CPD.TO","XIN.TO","XAW.TO","VSC.TO","VSB.TO","VGRO.TO","XUU.TO","ZEB.TO","ZCM.TO",
-"XFN.TO","ZDB.TO","VSP.TO","VEE.TO","VBAL.TO","CBO.TO","ZWP.TO"]
-
+with open ("./data/TSX_etfs_20200404.txt") as file:
+    tsx_etfs = [x.strip()+'.TO' for x in file]
 
 # %%
 
 d = {}
-for e in cad_etfs_top50_cap:
+for e in tsx_etfs:
     d[e] = yf.Ticker(e)
 
 # %%
@@ -32,9 +29,4 @@ for key in d:
     try:
         print('ticker:{0} div_yield:{1}'.format(d[key].info['symbol'], d[key].info['yield']))
     except:
-        print('{0} does not have data'.format(key))
-# %%
-k = yf.Ticker
-
-#t = yf.Tickers("XSP.TO XIC.TO")
-#dt = t.download()
+        print('Exception. {0} does not have data'.format(key))
